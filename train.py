523ba75e15a9cc9708a9cc4a2a25d364f0bae834
Geroff21 <geroffmc@gmail.com>
2024-12-02 12:37:50 +0300

    fix: add new path to train.py script

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ model = YOLO("yolov8n.pt")
 model.train(
     data="training_data/data.yaml", 
     imgsz=640,                      
-    epochs=4,                       
+    epochs=2,                       
     batch=8,                       
     name="yolov8n_custom",
     project="output"
